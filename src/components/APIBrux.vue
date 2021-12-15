6ec3806e14839fc207f5f6658dded3bc35d1fa84
NewOkNo <andrejshersh@gmail.com>
2021-12-15 23:50:25 +0200

    apiFix and notificationsAdded

--- src/components/APIBrux.vue
+++ src/components/APIBrux.vue
@@ -5,14 +5,38 @@
     </div>
 </template>
 <script>
-import { getCurrentPrice } from '../js/apijs.js';
+import { getPrice } from '../js/apijs.js';
+import {
+ isPushNotificationSupported,
+ sendNotification,
+ initializePushNotifications,
+ registerServiceWorker
+} from "../js/push-notifications.js";
 export default {
     methods: {
         getAPIinfo: function(){
-            getCurrentPrice.then((result) => {
-                console.log(result.data[0]);
-            });
+            var mode = 3;
+            //var param = {start:"2021-12-15T20%3A59%3A59.999Z", end:"2021-12-17T20%3A59%3A59.999Z"};
+            var param = "EE";
+            getPrice(mode, param).then((result) => {
+                console.log(result)
+                //console.log(result.data[0])
+                const pushNotificationSuported = isPushNotificationSupported();
+                if (pushNotificationSuported) {
+                    registerServiceWorker();
+                    initializePushNotifications().then(function(consent){
+                        if(consent === 'granted') {
+                        sendNotification();
+                        }
+                    });
+                }
+            })
+            /*window.setInterval(() => {
+                getLastPrice.then((result) => {
+                    console.log(result.data[0])
+                })
+            }, 1000*60*60);*/
         }
-    },    
+    },
 }
 </script>
--- src/js/apijs.js
+++ src/js/apijs.js
@@ -1,5 +1,5 @@
 //export async function getCurentWeather(){
-let getCurrentPrice = new Promise(async function(res, rej) {
+/*let getCurrentPrice = new Promise(async function(res, rej) {
   fetch("https://dashboard.elering.ee/api/nps/price/EE/current", {
     "method": "GET",
     "mode": "cors"
@@ -20,6 +20,67 @@ let getCurrentPrice = new Promise(async function(res, rej) {
     console.log(err);
   });
 });
+let getLastPrice = new Promise(async function(res, rej){
+  fetch("https://dashboard.elering.ee/api/nps/price/EE/latest", {
+    "method": "GET",
+    "mode": "cors"
+  })
+  .then(response => {
+    //console.log(response.json())
+    if(response.ok){
+      res(response.json());
+    } else{
+      alert("Server returned " + response.status + " : " + response.statusText);
+    }
+  })
+  .then(response => {
+    this.result = response.body;
+    this.responseAvailable = true;
+  })
+  .catch(err => {
+    console.log(err);
+  });
+})*/
+var getPrice = function(mode, param){
+  return new Promise(async function(res, rej){
+    var link = "https://dashboard.elering.ee/api/nps/price"
+    if(mode == 1){
+      link += "?start="+param.start+"&end="+param.end
+    }
+    else if(mode == 2){
+      link += "/"+param+"/current"
+    }
+    else if(mode == 3){
+      link += "/"+param+"/latest"
+    }
+    else{
+      rej("Wrong mode")
+    }
+    //console.log(link)
+    fetch(link, {
+      "method": "GET",
+      "mode": "cors"
+    })
+    .then(response => {
+      //console.log(response.json())
+      if(response.ok){
+        res(response.json());
+      } else{
+        rej("Server returned " + response.status + " : " + response.statusText);
+      }
+    })
+    .then(response => {
+      this.result = response.body;
+      this.responseAvailable = true;
+    })
+    .catch(err => {
+      rej(err);
+    });
+  })
+}
 export{
-  getCurrentPrice,
+  //getCurrentPrice,
+  //getLastPrice,
+  getPrice
 };
+//https://dashboard.elering.ee/api/nps/price?start=2021-12-15T20%3A59%3A59.999Z&end=2021-12-17T20%3A59%3A59.999Z
